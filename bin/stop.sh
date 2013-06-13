b7fffab573d68b39305803c9004cb1d2a09f7631
Benoit Verreault <benoit.verreault@riotgames.com>
2013-06-13 18:42:12 -0400

    Update configs

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,6 +1,6 @@
 pid=`ps aux | grep repose | grep sudo | awk {'print $2'}`
-sudo kill -2 $pid
-sudo kill -2 $pid
-sudo kill -2 $pid
-sudo kill -2 $pid
-sudo kill -2 $pid
+sudo kill -2 $pid &> /dev/null
+sudo kill -2 $pid &> /dev/null
+sudo kill -2 $pid &> /dev/null
+sudo kill -2 $pid &> /dev/null
+sudo kill -2 $pid &> /dev/null
